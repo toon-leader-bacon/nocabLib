320ca232f3655b116446b9aa0ac48df4201804ca
Arthur Bacon <ArthurBacon@NocabSoftware.com>
2021-08-14 13:09:28 -0400

    Add hash utility for primitive types

--- Utilities/MathHelpers/NocabHashUtility.cs
+++ Utilities/MathHelpers/NocabHashUtility.cs
@@ -5,8 +5,18 @@ using UnityEngine;
 public static class NocabHashUtility
 {
 
-  public static int generateHash(ICollection<object> definingElements)
-  {
+  public static int generateHash(ICollection<object> definingElements) {
+    int hash = 23;
+    int prime = 31;
+    foreach (object obj in definingElements) { hash = (prime * hash) + obj.GetHashCode(); }
+    return hash;
+  }
+
+  public static int generateHash<T>(ICollection<T> definingElements) where T : struct {
+    /**
+     * Used for when you need a hash value from a bunch of primitive types.
+     * @param definingElements A Collection of primitive (or struct) types.
+     */
     int hash = 23;
     int prime = 31;
     foreach (object obj in definingElements) { hash = (prime * hash) + obj.GetHashCode(); }
